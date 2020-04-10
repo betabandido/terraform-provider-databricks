907fd5cf97f51be26abcefb6edaca531f7af3fb1
Eugenio Marinetto <nenetto@gmail.com>
2020-04-10 14:27:21 +0200

    Add test script with logs

--- testing-terraform/test.sh
+++ testing-terraform/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+#rm terraform.tfstate crash.log
+export TF_LOG_PATH=testprovider.log
+export TF_LOG=DEBUG
+rm $TF_LOG_PATH
 terraform init && terraform plan && terraform apply
